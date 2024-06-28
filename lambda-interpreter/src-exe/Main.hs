b46f542462ce94c1ae0295043d5a89f2d3e714b8
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-06-28 19:59:36 +0200

    +equiv

--- lambda-interpreter/src-exe/Main.hs
+++ lambda-interpreter/src-exe/Main.hs
@@ -3,13 +3,13 @@ module Main where
 import Lambda
 import System.Console.Haskeline
 
-data Mode = REPEAT | REDUCE | STEPS | PRINT | SHOW deriving (Read, Show)
+data Mode = PRINT | REDUCE | STEPS | EQUIV | SHOW deriving (Read, Show)
 
 color :: String -> String -> String
 color typ str = "\ESC[" ++ typ ++ "m" ++ str ++ "\ESC[0m"
 
 io :: (Lambda -> String) -> Maybe Lambda -> InputT IO ()
-io _ Nothing = outputStrLn $ (color "1;31" "error:") ++ " incorrect syntax."
+io _ Nothing = outputStrLn $ color "1;31" "error:" ++ " incorrect syntax."
 io f (Just l) = outputStrLn $ f l
 
 help :: InputT IO ()
@@ -17,24 +17,34 @@ help = do
   outputStrLn "usage: [:rp | :rs | :r | :p | :s] [LAMBDA]"
 
 eval :: Mode -> String -> InputT IO ()
-eval REPEAT = outputStrLn . ("Input was: " ++)
-eval REDUCE = io (("> " ++) . color "34" . unparse' . reduce) . parse'
+eval PRINT = io (("| " ++) . color "34" . unparse') . parse'
+eval REDUCE = io (("| " ++) . color "34" . unparse' . reduce) . parse'
 eval STEPS = io' . parse'
   where
     io' :: Maybe Lambda -> InputT IO ()
-    io' Nothing = outputStrLn $ (color "1;31" "error:") ++ " Incorrect syntax."
+    io' Nothing = outputStrLn $ color "1;31" "error:" ++ " Incorrect syntax."
     io' (Just l) = showSteps l
     showSteps :: Lambda -> InputT IO ()
     showSteps l
       | found = do
-        outputStr $ "> " ++ color "34" (unparse' l')
+        outputStr $ "| " ++ color "34" (unparse' l')
         _ <- getInputLine ""
         showSteps l'
       | otherwise = return ()
       where
         (l', found) = reduceStep l
-eval PRINT = io (("> " ++) . color "33" . unparse') . parse'
-eval SHOW = io (("> " ++) . color "35" . show) . parse'
+eval EQUIV = showEquiv
+  where
+    io' :: Maybe Bool -> InputT IO ()
+    io' Nothing = outputStrLn $ color "1;31" "error:" ++ " Incorrect syntax."
+    io' (Just b) = outputStrLn . ("| " ++) . color "33" . show $ b
+    showEquiv :: String -> InputT IO ()
+    showEquiv str1 = do
+      let ml1 = parse' str1
+      str2 <- getInputLine $ "  (" ++ color "37" "AND" ++ ") > "
+      let ml2 = str2 >>= parse'
+      io' $ raise equiv' ml1 ml2
+eval SHOW = io (("| " ++) . color "35" . show) . parse'
 
 settings :: Settings IO
 settings = Settings {
@@ -44,11 +54,11 @@ settings = Settings {
 }
 
 main :: IO ()
-main = runInputT settings $ loop REPEAT
+main = runInputT settings $ loop REDUCE
   where
     loop :: Mode -> InputT IO ()
     loop mode = do
-      minput <- getInputLine $ "(\ESC[37m" ++ show mode ++ "\ESC[0m): "
+      minput <- getInputLine $ "(" ++ color "37" (show mode) ++ ") > "-- ++ color "37" ">" ++ " "
       case minput of
         Nothing -> return ()
         Just "" -> loop mode
@@ -56,19 +66,19 @@ main = runInputT settings $ loop REPEAT
         Just ":q" -> return ()
         Just ":help" -> help >> loop mode
         Just ":h" -> help >> loop mode
-        Just ":repeat" -> loop REPEAT
         Just ":steps" -> loop STEPS
         Just ":reduce" -> loop REDUCE
         Just ":print" -> loop PRINT
+        Just ":equiv" -> loop EQUIV
         Just ":show" -> loop SHOW
-        Just ":rp" -> loop REPEAT
         Just ":rs" -> loop STEPS
         Just ":r" -> loop REDUCE
         Just ":p" -> loop PRINT
+        Just ":eq" -> loop EQUIV
         Just ":s" -> loop SHOW
-        Just (':':'r':'p':' ':rest) -> eval REPEAT rest >> loop mode
         Just (':':'r':'s':' ':rest) -> eval STEPS rest >> loop mode
         Just (':':'r':' ':rest) -> eval REDUCE rest >> loop mode
         Just (':':'p':' ':rest) -> eval PRINT rest >> loop mode
+        Just (':':'e':'q':' ':rest) -> eval EQUIV rest >> loop mode
         Just (':':'s':' ':rest) -> eval SHOW rest >> loop mode
         Just input -> eval mode input >> loop mode
